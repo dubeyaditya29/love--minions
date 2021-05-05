2bfa778ac8cc248f07accbe7d893a5df5add7ae6
dubeyaditya29 <dubeyaditya890@gmail.com>
2021-05-05 11:57:10 +0530

    error handling added

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var output = document.querySelector("#output--area");
 function createURL(text) {
   return "https://api.funtranslations.com/translate/minion.json?text=" + text;
 }
-
+function catchError(error) {
+  console.error(error);
+  alert("something wrong with server!!");
+}
 button.addEventListener("click", () => {
   var url = createURL(input.value);
   fetch(url)
@@ -13,5 +16,6 @@ button.addEventListener("click", () => {
     .then((json) => json.contents.translated)
     .then((val) => {
       output.innerText = val;
-    });
+    })
+    .catch(catchError);
 });
